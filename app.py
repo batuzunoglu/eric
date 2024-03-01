61ff2956639fb7d8b952bea71634ac55aba337b4
Batuhan Uzunoglu <batuzunoglu@gmail.com>
2024-03-01 12:03:18 -0500

    app.py horse

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import matplotlib.pyplot as plt
+
+def at_ciz():
+    at = """
+         ,--,
+    _ ___/ /\|
+ ,;'( )__, )  ~
+//  //   '--;.
+'   \     | ^
+     ^    ^      
+    """
+    print(at)
+
+at_ciz()
