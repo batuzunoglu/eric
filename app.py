1989356fc6a5b9df4ef92b76b043e471ce799c32
Batuhan Uzunoglu <batuzunoglu@gmail.com>
2024-03-01 16:00:37 -0500

    flask

--- app.py
+++ app.py
@@ -15,3 +15,16 @@ def draw_horse():
     print(horse)
 
 draw_horse()
+
+from flask import Flask
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    print_message = "Hello, Render!"
+    print(print_message)
+    return print_message
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
